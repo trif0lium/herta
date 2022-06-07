18151cf731dbff13e148c85dfbfbcb16929c3000
trif0lium <gh@fleeingsunlight.dev>
2022-06-07 17:15:14 +0700

    feat(cmd/videotranscoder): TRANSCODING_TASK_QUEUE

---
+++ go/cmd/videotranscoder/main.go
@@ -0,0 +1,22 @@
+package videotranscoder
+
+import (
+	"github.com/trif0lium/herta/go/internal/workflows/videoprocessing"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
+
+func main() {
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "TRANSCODING_TASK_QUEUE", worker.Options{})
+	w.RegisterWorkflow(videoprocessing.TranscodingWorkflow)
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		panic(err)
+	}
+}
