ec8ef414d041dc396f469b4a1eb2a425ed5849df
trif0lium <gh@fleeingsunlight.dev>
2022-06-07 18:55:48 +0700

    feat(cmd/api): TranscodingWorkflow

--- go/cmd/api/main.go
+++ go/cmd/api/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/trif0lium/herta/go/internal/workflows/videoprocessing"
+	"go.temporal.io/sdk/client"
 )
 
 func main() {
@@ -13,9 +15,23 @@ func main() {
 	if len(PORT) == 0 {
 		PORT = "1323"
 	}
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
 	e := echo.New()
-	e.GET("/healthz", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]bool{
+	e.GET("/healthz", func(ctx echo.Context) error {
+		options := client.StartWorkflowOptions{
+			TaskQueue: "TRANSCODING_TASK_QUEUE",
+		}
+		_, err := c.ExecuteWorkflow(ctx.Request().Context(), options, videoprocessing.TranscodingWorkflow, videoprocessing.TranscodingJob{})
+		if err != nil {
+			return ctx.JSON(http.StatusOK, map[string]bool{
+				"ok": false,
+			})
+		}
+		return ctx.JSON(http.StatusOK, map[string]bool{
 			"ok": true,
 		})
 	})
