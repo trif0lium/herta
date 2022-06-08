c152cb8c8370f9fd3148f05f7afed2976a148e6e
trif0lium <gh@fleeingsunlight.dev>
2022-06-08 16:15:02 +0700

    feat(temporal): init

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './search/search.module';
 import { CalculatorModule } from './calculator/calculator.module';
+import { TemporalModule } from './temporal/temporal.module';
 
 @Module({
-  imports: [SearchModule, CalculatorModule],
+  imports: [SearchModule, CalculatorModule, TemporalModule],
   controllers: [AppController],
   providers: [AppService],
 })
