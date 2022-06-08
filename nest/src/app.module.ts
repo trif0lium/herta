5f39dcc933b24f37c253d014f8c9696c341ebf0e
trif0lium <gh@fleeingsunlight.dev>
2022-06-08 12:34:06 +0700

    feat(nest/calculator): init

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './search/search.module';
+import { CalculatorModule } from './calculator/calculator.module';
 
 @Module({
-  imports: [SearchModule],
+  imports: [SearchModule, CalculatorModule],
   controllers: [AppController],
   providers: [AppService],
 })
