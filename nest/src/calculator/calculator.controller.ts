3dac1784bc3c7b6b55da53c44a71b998ec5aad45
trif0lium <gh@fleeingsunlight.dev>
2022-06-08 15:03:40 +0700

    feat(calculator): CALCULATOR_TASK_QUEUE

--- nest/src/calculator/calculator.controller.ts
+++ nest/src/calculator/calculator.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { Worker } from '@temporalio/worker'
 
 @Controller('calculator')
-export class CalculatorController {}
+export class CalculatorController {
+  private worker: Worker
+  async onModuleInit() {
+    this.worker = await Worker.create({
+      taskQueue: 'CALCULATOR_TASK_QUEUE',
+      workflowsPath: require.resolve('./workflows')
+    })
+  }
+}
--- nest/src/search/search.controller.ts
+++ nest/src/search/search.controller.ts
@@ -3,11 +3,11 @@ import { Worker } from '@temporalio/worker'
 
 @Controller('search')
 export class SearchController implements OnModuleInit {
-  private worker: Worker
+  // private worker: Worker
   async onModuleInit() {
-    this.worker = await Worker.create({
-      taskQueue: 'SEARCH_TASK_QUEUE',
-      workflowsPath: require.resolve('./workflows')
-    })
+    // this.worker = await Worker.create({
+    //   taskQueue: 'SEARCH_TASK_QUEUE',
+    //   workflowsPath: require.resolve('./workflows')
+    // })
   }
 }
