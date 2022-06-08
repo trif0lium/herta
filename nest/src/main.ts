f3a9195ea29dfb33be9c6f9d92e5b848b00016ae
trif0lium <gh@fleeingsunlight.dev>
2022-06-08 19:05:04 +0700

    feat(main): connectMicroservice

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TemporalServer } from './temporal/server-temporal';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    strategy: new TemporalServer({
+      workerOptions: {
+        workflowsPath: require.resolve('./search/workflows'),
+        taskQueue: 'CALCULATOR_TASK_QUEUE'
+      }
+    })
+  })
   await app.listen(3000);
 }
 bootstrap();
