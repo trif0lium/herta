85ddba02392344c9c054dc8c32cc64d502537397
trif0lium <gh@fleeingsunlight.dev>
2022-06-08 13:57:59 +0700

    feat(calculator): fibonacci()

--- nest/src/calculator/calculator.service.ts
+++ nest/src/calculator/calculator.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ActivityInterface } from '@temporalio/activity';
 
 @Injectable()
-export class CalculatorService {}
+export class CalculatorService implements ICalculatorService {
+  fibonacci(n: string): Promise<string> {
+    return Promise.resolve('' + this._fibonacci(Number(n)))
+  }
+
+  private _fibonacci(n: number): number {
+    return n < 2 ? n : this._fibonacci(n - 1) + this._fibonacci(n - 2)
+  }
+
+}
+
+interface ICalculatorService {
+  fibonacci(n: string): Promise<string>
+}
+
+export interface ICalculatorActivity extends ICalculatorService, ActivityInterface { }
