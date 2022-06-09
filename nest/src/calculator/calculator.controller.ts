e43baec61db904932489f2ec7d4bc65c3a313fbe
trif0lium <gh@fleeingsunlight.dev>
2022-06-09 17:36:29 +0700

    feat(calculator): fibonacci()

--- nest/src/calculator/calculator.controller.ts
+++ nest/src/calculator/calculator.controller.ts
@@ -1,13 +1,23 @@
 import { Controller } from '@nestjs/common';
+import { Payload } from '@nestjs/microservices';
 import { Worker } from '@temporalio/worker'
+import { TemporalActivity } from 'src/temporal/server-temporal';
+import { CalculatorService } from './calculator.service';
 
 @Controller('calculator')
 export class CalculatorController {
+  constructor(private readonly calculatorService: CalculatorService) { }
+
   // private worker: Worker
   async onModuleInit() {
     // this.worker = await Worker.create({
     //   taskQueue: 'CALCULATOR_TASK_QUEUE',
     //   workflowsPath: require.resolve('./workflows')
     // })
+  }
+
+  @TemporalActivity()
+  async fibonacci(@Payload() n: string): Promise<string> {
+    return this.calculatorService.fibonacci(n)
   }
 }
